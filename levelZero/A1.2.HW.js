f605feb23e1b5bb21fd109c5ef37d89de76316d7
sayan4545 <sayanchatterjee3010@gmail.com>
2025-02-19 17:51:31 +0530

    HW

---
+++ levelZero/A1.2.HW.js
@@ -0,0 +1,17 @@
+let lastName = "balika"
+console.log(lastName)
+
+let oneChar = 'C'
+console.log(oneChar)
+
+let longStr = "Basics build strong foundations"
+console.log(longStr)
+
+let strThree = "Three"
+let anotherstr = strThree
+console.log(anotherstr)
+
+let firstPart = 'pqr'
+let secondPart = 'stu'
+let combined = firstPart+secondPart
+console.log(combined)
