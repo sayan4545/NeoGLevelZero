61edb84061166fb378d5dc9b0869ed5e2bff998a
sayan4545 <sayanchatterjee3010@gmail.com>
2025-02-22 06:47:59 +0530

    Added more home work codes

---
+++ levelZero/A1.3.HW_02.js
@@ -0,0 +1,30 @@
+let fruit1 = "apple"
+let fruit2 = "banana"
+let fruit3 = "orange"
+
+let fruits = `${fruit1} , ${fruit2}, ${fruit3}`
+console.log(fruits);
+
+let friend1 = "sarah"
+let friend2 = "Alex"
+let friend3 = "Jessica"
+
+let greet1 = `Hey, ${friend1}`
+let greet2 = `Hey, ${friend2}`
+let greet3 = `Hey, ${friend3}`
+
+console.log(greet1);
+console.log(greet2);
+console.log(greet3);
+
+let price1 = 5.99
+let price2 = 9.99
+let price3 = 12.99
+
+console.log(price1)
+console.log(price2);
+console.log(price3);
+
+
+
+
---
+++ levelZero/A1.4.HW_exercise01.js
@@ -0,0 +1,16 @@
+let destinationOne = "Jaipur"
+let destinationOneAttractionOne = "Hawai Mahal"
+let destinationOneAttractionTwo = "Amber Fort"
+let destinationOneAttractionThree = "City Palace"
+let destinationOneRating = 8.5
+let destinationOneVisitorsPerYear = 30000000
+let destinationOnedescription = "Jaipurr,the pink city, is known for magbnificant palaces, vibrant culture and exquisite handicrafts"
+console.log("Destination: ",destinationOne);
+console.log("The rating of the destination: ",destinationOneRating);
+console.log("The number of vistors per year: ",destinationOneVisitorsPerYear);
+console.log(`The popular attractions are ${destinationOneAttractionOne},${destinationOneAttractionTwo},${destinationOneAttractionThree}`);
+console.log(`The description of the destination: ${destinationOnedescription}`);
+
+
+
+
